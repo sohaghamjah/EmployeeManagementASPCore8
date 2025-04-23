a642d9602b32ab0c52f3035633b81c90e0dbf1d8
sohaghamjah <aymansohag.contact@gmail.com>
2025-04-23 16:38:25 +0600

    project initialize

---
+++ EmployeesManagement.Model/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagement.Model
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string Department { get; set; }
+
+        public string Designation { get; set; }
+
+    }
+}
---
+++ EmployeesManagement.Model/UserActivity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagement.Model
+{
+    public class UserActivity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public string? UpdatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
