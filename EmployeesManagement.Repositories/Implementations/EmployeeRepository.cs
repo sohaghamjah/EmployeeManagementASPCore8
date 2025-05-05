0e9524ec504d8ce1edd96c4109831a9456b570c1
sohaghamjah <aymansohag.contact@gmail.com>
2025-05-05 16:07:39 +0600

    new project create for repository employee repository create and apply

---
+++ EmployeesManagement.Repositories/Implementations/EmployeeRepository.cs
@@ -0,0 +1,52 @@
+﻿using EmployeesManagement.Data;
+using EmployeesManagement.Model;
+using EmployeesManagement.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagement.Repositories.Implementations
+{
+    internal class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetAllAsync() => await _context.Employees.ToListAsync();
+
+        public async Task<Employee> GetByIdAsync(int id) => await _context.Employees.FindAsync(id);
+
+        public async Task AddAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var emp = await _context.Employees.FindAsync(id);
+            if (emp != null)
+            {
+                _context.Employees.Remove(emp);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _context.Employees.AnyAsync(e => e.Id == id);
+        }
+    }
+}
---
+++ EmployeesManagement.Repositories/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using EmployeesManagement.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagement.Repositories.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetAllAsync();
+        Task<Employee> GetByIdAsync(int id);
+        Task AddAsync(Employee employee);
+        Task UpdateAsync(Employee employee);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
